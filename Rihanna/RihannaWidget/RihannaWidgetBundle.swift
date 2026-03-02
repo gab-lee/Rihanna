da39b205f1437975dd48acb80c452c168213dc63
Gab <53164692+gab-lee@users.noreply.github.com>
2026-03-02 21:42:46 +0800

    added widget target

---
+++ Rihanna/RihannaWidget/RihannaWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RihannaWidgetBundle.swift
+//  RihannaWidget
+//
+//  Created by Gabriel Lee on 2/3/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RihannaWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RihannaWidget()
+    }
+}
