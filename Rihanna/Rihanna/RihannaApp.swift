e11ba7d502af7dea839962ba4b6d1af4ea729837
Gab <53164692+gab-lee@users.noreply.github.com>
2026-03-02 21:31:27 +0800

    Initial project setup

---
+++ Rihanna/Rihanna/RihannaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RihannaApp.swift
+//  Rihanna
+//
+//  Created by Gabriel Lee on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct RihannaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
